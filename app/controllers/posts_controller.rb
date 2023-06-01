2bd4f36c0cf9cf2a275f540810ac89866034bb18
Nicole Vu <nicole.vu@kellogg.northwestern.edu>
2023-06-01 00:52:40 +0000

    Image attachments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,10 +3,13 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
     
+    # how to let other applications access the data?
     # alternative responses to requests other than HTML
-    # respond_to do |format|
-    #   format.html # implicitly renders posts/index.html.erb
-    # end
+    respond_to do |format| 
+      format.html # implicitly renders posts/index.html.erb
+    # format.json do
+    # render:json => @posts # gives you data in json format if you went to posts.json
+    end
   end
 
   def new
@@ -27,12 +30,12 @@ class PostsController < ApplicationController
     redirect_to "/posts"
   end
 
-  # before_action :allow_cors
-  # def allow_cors
-  #   response.headers['Access-Control-Allow-Origin'] = '*'
-  #   response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
-  #   response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
-  #   response.headers['Access-Control-Max-Age'] = '1728000'
-  # end
+  before_action :allow_cors # way to protect app so javascript code doesn't attack application (????)
+  def allow_cors
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
+    response.headers['Access-Control-Max-Age'] = '1728000'
+  end
 
 end
